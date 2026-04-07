eb563157456a9f70058b0c950bf1e8af9e66546d
Denis Yastrebov <yastrebdev@gmail.com>
2026-04-07 14:00:32 +0700

    feat: create function aggregate_candles

---
+++ utils/aggregate_candles.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import Literal
+
+CandleInterval = Literal["1m", "5m", "10m"]
+
+
+def aggregate_candle(
+    trades: list[dict],
+    candle_interval: CandleInterval
+) -> dict:
+    sort_trades_by_trade_time = sorted(trades, key=lambda t: t["T"])
+    first_trade = sort_trades_by_trade_time[0]
+    last_trade = sort_trades_by_trade_time[-1]
+
+    prices = [float(t["p"]) for t in trades]
+    low_price = min(prices)
+    high_price = max(prices)
+
+    volume = sum(float(t["q"]) for t in trades)
+    trade_count = len(trades)
+
+
+    return {
+        "symbol": first_trade["s"],
+        "start_time": first_trade["T"],
+        "end_time": last_trade["T"],
+        "first_trade_id": first_trade["t"],
+        "last_trade_id": last_trade["t"],
+        "open_price": float(first_trade["p"]),
+        "close_price": float(last_trade["p"]),
+        "low_price": low_price,
+        "high_price": high_price,
+        "candle_interval": candle_interval,
+        "volume": volume,
+        "trade_count": trade_count,
+        "created_at": datetime.now()
+    }
